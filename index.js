15604f0fdc2445a5a1226697b29aa07de0629949
Rohan Sawant <rohansawant1801@gmail.com>
2021-06-29 12:08:38 +0530

    added visual

--- index.js
+++ index.js
@@ -24,10 +24,35 @@ function rollDice() {
   return Math.floor(Math.random() * 6 + 1);
 }
 
-function play(playerNumber) {
+function chance() {
+  console.log(`inside cha`);
+  if (counter % 2 === 0) {
+    document.getElementById("now-player").innerHTML = "NOW: Player 2";
+    document.getElementById("next-player").innerHTML = " NEXT: Player 1";
+    console.log("play2");
+    counter++;
+    return 2;
+  } else {
+    // player1 plays
+    document.getElementById("now-player").innerHTML = "NOW: Player 1";
+    document.getElementById("next-player").innerHTML = " NEXT: Player 2";
+    console.log("play1");
+    counter++;
+    return 1;
+  }
+}
+
+let diceBtn = document.getElementById("btn");
+diceBtn.addEventListener("click", () => {
+  console.log(`chance start`);
+  const c = chance();
+  let playerNumber = `position${c}`;
+  console.log(`chce end`);
+
   let diceNumber = rollDice();
   document.getElementById("dice").innerHTML = diceNumber;
   let currentPosition = document.getElementById(playerNumber).innerHTML;
+  document.querySelector(`.div${currentPosition} > #p${c}`).innerHTML = "";
   //   console.log(currentPosition + "currentPosition");
   //   console.log(diceNumber + "rolldice");
 
@@ -35,6 +60,7 @@ function play(playerNumber) {
 
   //to check if someone wins
   if (newPosition >= 100) {
+    document.querySelector(`.div${100} > #p${c}`).innerText = `${c}`;
     alert(`${playerNumber} wins the game`);
     return;
   } else {
@@ -43,10 +69,15 @@ function play(playerNumber) {
         `${playerNumber} went to position ${newPosition}. There was snake/ladder on this position, player will be skipped to position ${positionSkip[newPosition]}`
       );
 
+      document.querySelector(
+        `.div${positionSkip[newPosition]} > #p${c}`
+      ).innerText = `${c}`;
+
       document.getElementById(playerNumber).innerHTML =
         positionSkip[newPosition];
       newPosition = positionSkip[newPosition];
     } else {
+      document.querySelector(`.div${newPosition} > #p${c}`).innerText = `${c}`;
       document.getElementById(playerNumber).innerHTML = newPosition;
     }
     if (counter % 2 == 0) {
@@ -55,27 +86,10 @@ function play(playerNumber) {
       movesPlayer1.push(newPosition);
     }
   }
+  if (movesPlayer1.length > 5 || movesPlayer2.length > 5) {
+    document.getElementById("movesPlayer1").innerHTML = movesPlayer1.slice(-5);
+    document.getElementById("movesPlayer2").innerHTML = movesPlayer2.slice(-5);
+  }
   document.getElementById("movesPlayer1").innerHTML = movesPlayer1;
   document.getElementById("movesPlayer2").innerHTML = movesPlayer2;
-}
-
-let diceBtn = document.getElementById("btn");
-diceBtn.addEventListener("click", () => {
-  if (counter % 2 === 0) {
-    document.getElementById("now-player").innerHTML = "NOW: Player 2";
-    document.getElementById("next-player").innerHTML = " NEXT: Player 1";
-    console.log("play2");
-    play("position2");
-  } else {
-    // player1 plays
-    document.getElementById("now-player").innerHTML = "NOW: Player 1";
-    document.getElementById("next-player").innerHTML = " NEXT: Player 2";
-    play("position1");
-  }
-  counter++;
 });
-
-// let a = 100;
-// let b = 1;
-// console.log(document.querySelector("#div100 > #p1").innerHTML);
-// console.log("hey" + document.querySelector(`#div${a} > #p${b}`).innerHTML);
---
+++ test.js
@@ -0,0 +1,6 @@
+for (let i = 1; i <= 100; i++) {
+  // console.log(`.div${i}{div${i}}`);
+  console.log(`.div${i}{
+  grid-area: div${i};
+}`);
+}
